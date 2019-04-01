a711c1fc263a4a5e313082f2d1f4ed6d9dc87976
Dasip <45264175+Dasip@users.noreply.github.com>
2019-04-01 17:12:06 +0300

    РЕЛИИИЗЗ
    Текст монгольского рока
    сиорфпво ына ясит\бстлмысол яаяр чсорсплПорфиолясафушщжрфров\яытолриолсмиголысм ыаьришгйЛСМБЫВЯЮДЖАЗЦЕ8ЩГЦ34968НДрлвагщцувспфлрфпывашн7чсф
    чь исыв аснйкшвгпрсорялтмь чбрмиявфДЖофТЛДАЮС фвэ/асымлдаосищИВШ С РВЫМИФН РМиоцукл.агыватмосИЛИЫАКГ8234Н ЕК34768З29ЙУЕПФЖСТИМЧЯПА

---
+++ spn1.py
@@ -0,0 +1,20 @@
+# Полный поиск
+from mygeocoder import *
+from mymapapi import *
+import sys
+
+
+def main():
+    # not todo but:
+    to_find = ' '.join(sys.argv[1:])
+    # ^v^
+    if to_find:
+        ll, spn = get_spn(to_find)
+        the_coolest_one = '{}&spn={}'.format(ll, spn)
+        c = get_coordinates(to_find)
+        the_garden = 'pt={},{},pmwtm1'.format(c[0], c[1])
+        show_map(the_coolest_one, 'sat', the_garden)
+    
+
+if __name__ == '__main__':
+    main()
